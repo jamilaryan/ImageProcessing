d477cefd2f6e0d5bc3ca4239d1fab714cce7e4b2
Jamil <jamil@jamils-macbook-pro.local>
2016-02-05 14:03:41 -0500

    Initial Commit

---
+++ Cartoonifyme/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Cartoonifyme
+//
+//  Created by Jamil on 2016-02-05.
+//  Copyright © 2016 Jamil. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
